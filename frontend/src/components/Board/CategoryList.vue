3571b63e4c80816b742239d0b711af22428ad622
skymir-git <seratoa@gmail.com>
2020-04-27 14:04:38 +0900

    Vue page navigation apply.

--- frontend/src/components/Board/CategoryList.vue
+++ frontend/src/components/Board/CategoryList.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="container">
-        <div style="width: 80%; padding: 20px; text-align: center;">
+        <div style="padding: 20px; text-align: center;">
             <table class="table table-hover">
                 <thead>
                 <tr>
@@ -13,14 +13,36 @@
                 <tr v-if="boardList === null">
                     <td colspan="3" style="text-align: center">데이터가 없습니다</td>
                 </tr>
-                <tr v-else v-for="(board, bIdx) in boardList" :key="bIdx"
+                <tr v-else v-for="(board, bIdx) in displayedPosts" :key="bIdx"
                     @click="detail(board.post_id)">
-                    <th scope="row">{{bIdx + 1}}</th>
+                    <th scope="row">{{board.post_id}}</th>
                     <td>{{board.title}}</td>
                     <td>{{board.views}}</td>
                 </tr>
                 </tbody>
             </table>
+
+            <div id="pageStyle">
+                <nav aria-label="Page navigation">
+                    <ul class="pagination">
+                        <li class="page-item">
+                            <button type="button" class="page-link" v-if="pageNum !== 1" @click="pageNum--"> 이전</button>
+                        </li>
+                        <li class="page-item">
+                            <button type="button" class="page-link"
+                                    v-for="pageNumber in totalPage.slice(pageNum-1, pageNum+4)" :key="pageNumber"
+                                    @click="pageNum = pageNumber"> {{pageNumber}}
+                            </button>
+                        </li>
+                        <li class="page-item">
+                            <button type="button" @click="pageNum++" v-if="pageNum < totalPage.length"
+                                    class="page-link"> 다음
+                            </button>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+
             <button type="button" class="float-right btn btn-success" @click="post()">글쓰기</button>
         </div>
     </div>
@@ -33,25 +55,53 @@
         },
         data() {
             return {
+                pageNum: 1,
+                pageSize: 10,
+                totalPage: [],
                 categoryId: null,
                 boardList: null,
-                pageArray: null,
             }
         },
-        mounted() {
-            this.$http.get(`/api/board/${this.categoryName}`)
-                .then((res) => {
-                    if (res.data.length === 0) {
-                        this.category()
-                        return
-                    }
-                    this.boardList = res.data
-                    this.categoryId = res.data[0].ca_id
-                }).catch(function (error) {
-                console.log(`Error : ${error}`)
-            });
+        created() {
+            this.getPosts()
+        },
+        computed: {
+            displayedPosts() {
+                return this.paginate();
+            }
+        },
+        watch: {
+            boardList() {
+                this.setPages();
+            }
         },
         methods: {
+            getPosts() {
+                this.$http.get(`/api/board/${this.categoryName}`)
+                    .then((res) => {
+                        if (res.data.length === 0) {
+                            this.category()
+                            return
+                        }
+                        this.boardList = res.data
+                        this.categoryId = res.data[0].ca_id
+                    }).catch(function (error) {
+                    console.log(`Error : ${error}`)
+                });
+            },
+            setPages() {
+                let numberOfPages = Math.ceil(this.boardList.length / this.pageSize);
+                for (let index = 1; index <= numberOfPages; index++) {
+                    this.totalPage.push(index);
+                }
+            },
+            paginate() {
+                let page = this.pageNum;
+                let pageSize = this.pageSize;
+                let from = (page * pageSize) - pageSize;
+                let to = (page * pageSize);
+                return this.boardList.slice(from, to);
+            },
             category() {
                 this.$http.get(`/api/board/info/${this.categoryName}`)
                     .then((res) => {
@@ -62,14 +112,30 @@
             },
             detail(seq) {
                 this.$http.get(`/api/board/views/${seq}`)
-                this.$router.push(`article/${seq}`)
+                this.$router.push(`/article/${seq}/`)
             },
             post() {
                 this.$router.push({
                     name: 'Posting',
                     params: {board_id: 1, categoryName: this.categoryName, categoryId: this.categoryId, att: 'post'}
                 })
-            }
+            },
         }
     }
 </script>
+
+<style>
+    #pageStyle {
+        float: left;
+    }
+
+    button.page-link {
+        display: inline-block;
+    }
+
+    button.page-link {
+        font-size: 20px;
+        color: #29b3ed;
+        font-weight: 500;
+    }
+</style>
