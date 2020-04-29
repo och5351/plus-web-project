414587eea3ec86b279c40621551fd1d9e20390d6
skymir-git <seratoa@gmail.com>
2020-04-29 18:28:59 +0900

    component 분리

---
+++ frontend/src/components/Board/CategoryFooter.vue
@@ -0,0 +1,136 @@
+<template>
+    <div>
+        <!-- Footer -->
+        <footer class="footer">
+            <!-- Footer Links -->
+            <div class="container text-center text-md-left">
+
+                <!-- Grid row -->
+                <div class="row">
+
+                    <!-- Grid column -->
+                    <div class="col-md-4 mx-auto text-info">
+
+                        <!-- Content -->
+                        <h5 class="font-weight-bold text-uppercase mt-3 mb-4">바로가기</h5>
+                        <p>Here you can use rows and columns to organize your footer content. Lorem ipsum dolor sit
+                            amet,
+                            consectetur
+                            adipisicing elit.</p>
+
+                    </div>
+                    <!-- Grid column -->
+
+                    <hr class="clearfix w-100 d-md-none">
+
+                    <!-- Grid column -->
+                    <div class="col-md-2 mx-auto text-dark">
+                        <!-- Links -->
+                        <h5 class="font-weight-bold text-uppercase mt-3 mb-4">Game</h5>
+                        <ul class="list-unstyled">
+                            <li>
+                                <a href="#!">League Of Legend</a>
+                            </li>
+                            <li>
+                                <a href="#!">Battle Ground</a>
+                            </li>
+                            <li>
+                                <a href="#!">Sudden Attack</a>
+                            </li>
+                            <li>
+                                <a href="#!">Maple Stroy</a>
+                            </li>
+                        </ul>
+
+                    </div>
+                    <!-- Grid column -->
+
+                    <hr class="clearfix w-100 d-md-none">
+
+                    <!-- Grid column -->
+                    <div class="col-md-2 mx-auto text-dark">
+                        <!-- Links -->
+                        <h5 class="font-weight-bold text-uppercase mt-3 mb-4">Music</h5>
+                        <ul class="list-unstyled">
+                            <li>
+                                <a href="#!">Ballade</a>
+                            </li>
+                            <li>
+                                <a href="#!">Hip Hop</a>
+                            </li>
+                            <li>
+                                <a href="#!">Jazz</a>
+                            </li>
+                            <li>
+                                <a href="#!">PoP</a>
+                            </li>
+                        </ul>
+
+                    </div>
+                    <!-- Grid column -->
+
+                    <hr class="clearfix w-100 d-md-none">
+
+                    <!-- Grid column -->
+                    <div class="col-md-2 mx-auto text-dark">
+
+                        <!-- Links -->
+                        <h5 class="font-weight-bold text-uppercase mt-3 mb-4">Food</h5>
+
+                        <ul class="list-unstyled">
+                            <li>
+                                <a href="#!">Korean</a>
+                            </li>
+                            <li>
+                                <a href="#!">Western food</a>
+                            </li>
+                            <li>
+                                <a href="#!">Instant</a>
+                            </li>
+                            <li>
+                                <a href="#!">Junk food</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <!-- Grid column -->
+                </div>
+                <!-- Grid row -->
+            </div>
+
+            <!-- Social buttons -->
+            <ul class="list-unstyled list-inline text-center">
+                <li class="list-inline-item">
+                    <i class="fab fa-facebook-f"> </i>
+                </li>
+                <li class="list-inline-item">
+                    <i class="fab fa-twitter"> </i>
+                </li>
+                <li class="list-inline-item">
+                    <i class="fab fa-google-plus-g"> </i>
+                </li>
+                <li class="list-inline-item">
+                    <i class="fab fa-github"> </i>
+                </li>
+            </ul>
+
+            <!-- Copyright -->
+            <div class="footer-copyright text-center py-3">© 2020 Copyright:
+                <a href="https://plus-market.com">PLUS</a>
+            </div>
+
+        </footer>
+        <!-- Footer -->
+    </div>
+</template>
+
+<style>
+    .footer{
+        margin-top: 100px;
+        color:black;
+    }
+
+    span {
+        font-size: 14px;
+        -webkit-text-fill-color: gray;
+    }
+</style>
