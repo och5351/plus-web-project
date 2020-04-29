d51d6ba78791e55f4dca6b556292a2ae5dda7f4b
och5351 <dhcksgo5319@naver.com>
2020-04-29 12:04:12 +0900

    Update CLSTM

--- CLSTM_model_with_python/preprosess/StopWords.py
+++ CLSTM_model_with_python/preprosess/StopWords.py
@@ -6,7 +6,7 @@ class StopWords:
 
     # 불용어 사전 업로드
     def load_stopWordsDic(self):
-        f = open('C:/Users/och5351/Desktop/github_och/plus-web-project/CLSTM_model_with_python/stopwordsKorean.txt',
+        f = open('stopwordsKorean.txt',
                  'r', -1, "utf-8")
         data = f.readlines()
         f.close()
