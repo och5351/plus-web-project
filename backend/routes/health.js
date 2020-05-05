6d7ac2972211b2168cdd6bd2c6ce037b5488dff5
sdnmd <sdnmd98@naver.com>
2020-05-05 10:00:59 +0900

    modified app.js health code

--- backend/routes/health.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET health status server */
-router.get('/health', function(req, res) {
-    res.status(200).send();
-});
-
-module.exports = router;
