ab4a0c2943501ee7f1f0e28732f1500c979f2349
skymir-git <seratoa@gmail.com>
2020-05-05 23:32:24 +0900

    ESLint parser change.

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -27,7 +27,6 @@ module.exports = {
 		],
 		'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
 	},
-	parser: 'vue-eslint-parser',
 	parserOptions: {
 		parser: 'babel-eslint',
 		sourceType: 'module',
