b140877582df0bdd348ed2b46cf56f78b788ed46
sdnmd <sdnmd98@naver.com>
2020-05-12 15:03:27 +0900

    commit

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -6,5 +6,9 @@ router.get('/', function(req, res, next) {
     res.render('index', { title: 'Express' });
 });
 
+/* GET health status server */
+router.get('/health', function(req, res) {
+    res.status(200).send();
+});
 
 module.exports = router;
