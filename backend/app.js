5cff88d64d797e373861d99fb00fee4e3d0d5be8
och5351 <dhcksgo5319@naver.com>
2020-06-27 17:40:47 +0900

    chaging ignore

--- backend/app.js
+++ backend/app.js
@@ -15,6 +15,8 @@ const articleRouter = require("./routes/article");
 
 const app = express();
 
+app.use(require('connect-history-api-fallback')());
+
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
@@ -35,19 +37,19 @@ app.use("/api/post", postRouter);
 app.use("/api/article", articleRouter);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
+app.use(function(req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
+    // render the error page
+    res.status(err.status || 500);
+    res.render("error");
 });
 
 module.exports = app;
