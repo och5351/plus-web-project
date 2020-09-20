a56be3f8f19436e51184fc2da8077877ca094cb6
cmhxone <cmhxone@outlook.kr>
2020-09-21 03:13:22 +0900

    Make FileUploading Directory if not exist

--- backend/lib/fileupload.js
+++ backend/lib/fileupload.js
@@ -1,8 +1,11 @@
+const fs = require('fs');
+
 var fileupload = {};
 
 fileupload.path = __dirname + '/../public/uploads/';
-// 로컬 Devel용 위치
-// fileupload.path = __dirname + '/../../frontend/public/uploads/';
 fileupload.currentDir = __dirname;
+
+fs.mkdir(fileupload.path, err => {
+})
 
 module.exports = fileupload;
