9bee19f375f3cc334b68c7d467b53d887354382e
cmhxone <cmhxone@outlook.kr>
2020-09-27 05:22:38 +0900

    Google OAuth2.0 Testing

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const passport = require('../lib/gauth');
+const oauth2Client = require('../lib/gauth');
 
 // mysql 선언
 var dbConObj = require('../lib/db_config');
@@ -8,7 +8,18 @@ var conn = dbConObj.init();
 
 // Google OAuth2.0
 router.post('/google', function(req, res, next) {
-    
+    var code = req.body.code;
+    var redirect_uri = req.body.redirect_uri;
+
+    const {tokens} = await oauth2Client.getToken(code);
+    oauth2Client.setCredentials(tokens);
+
+    oauth2Client.on('tokens', (tokens) => {
+        if (tokens.refresh_token) {
+            console.log(tokens.refresh_token);
+        }
+        console.log(token.access_token);
+    })
 });
 
 module.exports = router;
