d9eb989a63ace72a47fdba609764b8c54d00b67d
cmhxone <cmhxone@outlook.kr>
2020-09-28 00:21:04 +0900

    Backend Testing

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,13 +1,33 @@
 const express = require('express');
 const router = express.Router();
 
+const { OAuth2Client } = require('google-auth-library');
+
+// 구글 OAuth 정보
+const googleOAuthInfo = require('../lib/gauth');
+
 // mysql 선언
 var dbConObj = require('../lib/db_config');
 var conn = dbConObj.init();
 
+//=====================================================================//
+const client = new OAuth2Client(
+    googleOAuthInfo.clientID,
+    googleOAuthInfo.clientSecret,
+    googleOAuthInfo.callbackURL,
+)
+//=====================================================================//
+
 // Google OAuth2.0
 router.post('/google', function(req, res, next) {
-    console.info(req.body);
+    async function verify() {
+        const ticket = await client.verifyIdToken({
+            idToken: req.body.code
+        });
+
+        return ticket;
+    }
+    verify().then((ticket) => {console.log(ticket)}).catch(console.error);
 });
 
 module.exports = router;
