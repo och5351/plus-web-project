607a33060b10d79a709a92e66f0ec6529b254287
cmhxone <cmhxone@outlook.kr>
2020-09-27 23:57:42 +0900

    Backend Testing

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const oauth2Client = require('../lib/gauth');
 
 // mysql 선언
 var dbConObj = require('../lib/db_config');
@@ -8,18 +7,7 @@ var conn = dbConObj.init();
 
 // Google OAuth2.0
 router.post('/google', function(req, res, next) {
-    var code = req.body.code;
-    var redirect_uri = req.body.redirect_uri;
-
-    const {tokens} = oauth2Client.getToken(code);
-    oauth2Client.setCredentials(tokens);
-
-    oauth2Client.on('tokens', (tokens) => {
-        if (tokens.refresh_token) {
-            console.log(tokens.refresh_token);
-        }
-        console.log(token.access_token);
-    })
+    console.info(req.body);
 });
 
 module.exports = router;
