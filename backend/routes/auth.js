a2ba30e9d5c025d1786ed3923f7c2ab42fcdb47f
cmhxone <cmhxone@outlook.kr>
2020-09-29 09:17:02 +0900

    Test

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,27 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-const passport = require('passport');
-var GoogleStrategy = require('passport-google-oauth20').Strategy;
-
-// 구글 OAuth 정보
-const googleOAuthInfo = require('../lib/gauth');
-
 // mysql 선언
 var dbConObj = require('../lib/db_config');
 var conn = dbConObj.init();
-
-//=====================================================================//
-passport.use(new GoogleStrategy({
-        clientID: googleOAuthInfo.clientID,
-        clientSecret: googleOAuthInfo.clientSecret,
-        callbackURL: googleOAuthInfo.callbackURL,
-    },
-    function(accessToken, refreshToken, profile, cb) {
-        return cb(null, null);
-    }
-));
-//=====================================================================//
 
 router.get('/google', passport.authenticate('google', { scope: ['profile'], session: false }));
 
@@ -31,11 +13,11 @@ router.post('/google', function(req, res, next) {
     res.send(req.body.code);
 });
 
-router.get('/google/callback', passport.authenticate('google', { failureRedirect: '/login' }),
-    function(req, res) {
-        console.info(req.body);
-        res.redirect('http://localhost:8080/?token=' + res.req.user);
-    }
-);
+// router.get('/google/callback', passport.authenticate('google', { failureRedirect: '/login' }),
+//     function(req, res) {
+//         console.info(req.body);
+//         res.redirect('http://localhost:8080/?token=' + res.req.user);
+//     }
+// );
 
 module.exports = router;
