8ba0f97a94bb3cc8969e645c6866918d5280fd5b
cmhxone <cmhxone@outlook.kr>
2020-09-29 09:22:46 +0900

    Testing...

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,6 +1,21 @@
 const express = require('express');
 const router = express.Router();
 
+const GAuth = require('../lib/gauth');
+const passport = require('passport');
+var GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+passport.use(new GoogleStrategy({
+        clientID: GAuth.clientID,
+        clientSecret: GAuth.clientSecret,
+        callbackURL: GAuth.callbackURL,
+    },
+    function (accessToken, refreshToken, profile, callback) {
+        console.info(profile);
+        return callback(null, null);
+    }
+));
+
 // mysql 선언
 var dbConObj = require('../lib/db_config');
 var conn = dbConObj.init();
@@ -8,10 +23,10 @@ var conn = dbConObj.init();
 router.get('/google', passport.authenticate('google', { scope: ['profile'], session: false }));
 
 // Google OAuth2.0
-router.post('/google', function(req, res, next) {
-    console.info(req);
-    res.send(req.body.code);
-});
+// router.post('/google', function(req, res, next) {
+//     console.info(req);
+//     res.send(req.body.code);
+// });
 
 // router.get('/google/callback', passport.authenticate('google', { failureRedirect: '/login' }),
 //     function(req, res) {
